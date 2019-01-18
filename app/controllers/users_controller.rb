ab1375e963ad2d9bf29ab352111702c63054bd85
Omololu Oshoyemi <omololuadebowale1@gmail.com>
2019-01-18 10:27:37 +0000

    Add edit update fucntionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,22 @@ def create
  else
    render 'new'
  end
+end
+
+def edit
+ @user = User.find(params[:id])
+end
+
+def update
+ @user = User.find(params[:id])
+ if @user.update(user_params)
+  flash[:success] = "Your account was updated successfuly"
+  redirect_to articles_path
    
-   
+ else
+   render 'edit'
+ end
+  
 end
 
 private
